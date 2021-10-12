aae08f8d36b0a0e64eabc86a94d4be830628508f
Kristjan <voolaidk@tlu.ee>
2021-10-12 19:20:25 +0300

    import endpoint excel faili vastuvõtmiseks

---
+++ api/controllers/excelImportController.js
@@ -0,0 +1,12 @@
+const excelImportController = {};
+
+excelImportController.saveExcel = (req, res) => {
+    // TODO: Teha eraldi service faili jaoks? Hetkel lihtsalt testimiseks
+    console.log(req.files)
+
+    res.status(200).json({
+        msg: "Success"
+    })
+}
+
+module.exports = excelImportController;
