3274a99b9102036c4ad3ffb14c0bd9d9273abd79
Kristjan <voolaidk@tlu.ee>
2021-10-31 12:12:15 +0200

    mock db

--- database.js
+++ database.js
@@ -20,27 +20,113 @@ const database = {
   candidates: [
     {
       id: 1,
+      specialityCode: 'RIF21',
       firstName: 'John',
       lastName: 'Smith',
       email: 'john.smith@gmail.com',
       personalId: '39199876789',
-      score: 89
+      finalScore: 89,
+      scores: {
+        kat1: 15,
+        kat2: 10,
+        kat3: 20,
+        kat4: 44
+      },
+      studies: {
+        0: 'Science',
+        1: 'IT',
+        2: 'Sports'
+      },
+      residence: 'Tallinn',
+      phoneNumber: 567833091
     },
     {
       id: 2,
+      specialityCode: 'RIF21',
       firstName: 'Uku',
       lastName: 'Lele',
       email: 'uku.lele@gmail.com',
       personalId: '39199276789',
-      score: 89
+      finalScore: 89,
+      scores: {
+        kat1: 15,
+        kat2: 10,
+        kat3: 20,
+        kat4: 44
+      },
+      studies: {
+        0: 'Science',
+        1: 'IT',
+        2: 'Sports'
+      },
+      residence: 'Tallinn',
+      phoneNumber: 567833091
     },
     {
       id: 3,
+      specialityCode: 'RIF21',
       firstName: 'Mati',
       lastName: 'Tati',
       email: 'mati.tati@gmail.com',
       personalId: '39129276789',
-      score: 89
+      finalScore: 89,
+      scores: {
+        kat1: 15,
+        kat2: 10,
+        kat3: 20,
+        kat4: 44
+      },
+      studies: {
+        0: 'Science',
+        1: 'IT',
+        2: 'Sports'
+      },
+      residence: 'Tallinn',
+      phoneNumber: 567833091
+    },
+    {
+      id: 4,
+      specialityCode: 'KT21',
+      firstName: 'Mati',
+      lastName: 'Tati',
+      email: 'mati.tati@gmail.com',
+      personalId: '39129276789',
+      finalScore: 89,
+      scores: {
+        kat1: 15,
+        kat2: 10,
+        kat3: 20,
+        kat4: 44
+      },
+      studies: {
+        0: 'Science',
+        1: 'IT',
+        2: 'Sports'
+      },
+      residence: 'Tallinn',
+      phoneNumber: 567833091
+    },
+    {
+      id: 5,
+      specialityCode: 'KT21',
+      firstName: 'Mati',
+      lastName: 'Tati',
+      email: 'mati.tati@gmail.com',
+      personalId: '39129276789',
+      finalScore: 89,
+      scores: {
+        kat1: 15,
+        kat2: 10,
+        kat3: 20,
+        kat4: 44
+      },
+      studies: {
+        0: 'Science',
+        1: 'IT',
+        2: 'Sports'
+      },
+      residence: 'Tallinn',
+      phoneNumber: 567833091
     },
   ],
 };
